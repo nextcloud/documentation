b707401d442453c505c1e0f1744b2cb0dcc72f36
Daniel Molkentin <daniel@molkentin.de>
2013-10-21 20:11:45 +0200

    Work properly with subdirs

--- go.php
+++ go.php
@@ -4,10 +4,10 @@
 ############## Adjust when editing docs ##################
 
 $mapping = array(
-               'ldap' => 'admin_manual/auth_ldap.html',
-               'dir_permissions' => 'admin_manual/installation/installation_source.html#set-the-directory-permissions',
-               'source_install' => 'admin_manual/installation/installation_source.html',
-               'install' => 'admin_manual/installation.html',
+               'ldap'              => '/admin_manual/auth_ldap.html',
+               'dir_permissions'   => '/admin_manual/installation/installation_source.html#set-the-directory-permissions',
+               'source_install'    => '/admin_manual/installation/installation_source.html',
+               'install'           => '/admin_manual/installation.html',
            );
 
 ############# Do not edit below this line #################
@@ -17,7 +17,7 @@ $proto = isset($_SERVER['HTTPS']) ? 'https' : 'http';
 $port = $_SERVER['SERVER_PORT'];
 $port = ($port !== '80' && $port !== '443') ? ":$port" : '';
 $name = $_SERVER['SERVER_NAME'];
-$path = dirname($_SERVER['PATH_INFO']);
+$path = dirname($_SERVER['REQUEST_URI']);
 
 if (array_key_exists($from, $mapping)) {
     $target = $mapping[$from];
@@ -25,7 +25,8 @@ if (array_key_exists($from, $mapping)) {
     $target = '';
 }
 
-$location = "$proto://$name$port/$path$target";
+$location = "$proto://$name$port$path$target";
 
-header ('HTTP/1.1 302 Moved Temporarily');
-header ('Location: '.$location);
+header('HTTP/1.1 302 Moved Temporarily');
+header('Location: '.$location);
+
