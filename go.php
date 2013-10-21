303abc1c8b416c21ac873f75bf970847b7a4698b
Daniel Molkentin <daniel@molkentin.de>
2013-10-21 19:46:56 +0200

    Add a redirector script

---
+++ go.php
@@ -0,0 +1,31 @@
+<?php
+
+############## Add new references here  ##################
+############## Adjust when editing docs ##################
+
+$mapping = array(
+               'ldap' => 'admin_manual/auth_ldap.html',
+               'dir_permissions' => 'admin_manual/installation/installation_source.html#set-the-directory-permissions',
+               'source_install' => 'admin_manual/installation/installation_source.html',
+               'install' => 'admin_manual/installation.html',
+           );
+
+############# Do not edit below this line #################
+
+$from = $_GET['to'];
+$proto = isset($_SERVER['HTTPS']) ? 'https' : 'http';
+$port = $_SERVER['SERVER_PORT'];
+$port = ($port !== '80' && $port !== '443') ? ":$port" : '';
+$name = $_SERVER['SERVER_NAME'];
+$path = dirname($_SERVER['PATH_INFO']);
+
+if (array_key_exists($from, $mapping)) {
+    $target = $mapping[$from];
+} else {
+    $target = '';
+}
+
+$location = "$proto://$name$port/$path$target";
+
+header ('HTTP/1.1 302 Moved Temporarily');
+header ('Location: '.$location);
