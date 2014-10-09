f0e76cf1d118068876e179c4763745b8dabcc7d2
Lukas Reschke <lukas@owncloud.com>
2014-10-09 15:49:06 +0200

    Add some more keys
    Required for https://github.com/owncloud/core/pull/11498

--- go.php
+++ go.php
@@ -8,11 +8,17 @@ $mapping = array(
                'admin-dir_permissions'   => '/admin_manual/installation/installation_source.html#set-the-directory-permissions',
                'admin-source_install'    => '/admin_manual/installation/installation_source.html',
                'admin-install'           => '/admin_manual/installation/index.html',
+               'admin-encryption'        => '/admin_manual/configuration/configuration_encryption.html',
+               'admin-external-storage'  => '/admin_manual/configuration/custom_mount_config_gui.html',
 
                'user-webdav'             => '/user_manual/files/files.html',
                'user-manual'             => '/user_manual',
                'user-sync-calendars'     => '/user_manual/pim/calendar.html#synchronising-calendars-with-caldav',
                'user-sync-contacts'      => '/user_manual/pim/contacts.html#keeping-your-addressbook-in-sync',
+               'user-encryption'         => '/user_manual/files/encryption.html',
+               'user-trashbin'           => '/user_manual/files/deletedfiles.html',
+               'user-files'              => '/user_manual/files/index.html',
+               'user-versions'           => '/user_manual/files/versioncontrol.html'
            );
 
 ############# Do not edit below this line #################
