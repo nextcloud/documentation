d067dd11770242a005862f25c9e5a8b95c168b4f
Joas Schilling <nickvergessen@gmx.de>
2015-02-05 14:45:47 +0100

    Update the go admin doc links

--- go.php
+++ go.php
@@ -4,22 +4,22 @@
 ############## Adjust when editing docs ##################
 
 $mapping = array(
-               'admin-ldap'              => '/admin_manual/configuration/auth_ldap.html',
-               'admin-dir_permissions'   => '/admin_manual/installation/installation_source.html#setting-secure-directory-permissions',
-               'admin-source_install'    => '/admin_manual/installation/installation_source.html',
-               'admin-install'           => '/admin_manual/installation/index.html',
-               'admin-encryption'        => '/admin_manual/configuration/configuration_encryption.html',
-               'admin-external-storage'  => '/admin_manual/configuration/custom_mount_config_gui.html',
+    'admin-ldap'              => '/admin_manual/configuration/user_auth_ldap.html',
+    'admin-dir_permissions'   => '/admin_manual/installation/installation_wizard.html#setting-strong-directory-permissions',
+    'admin-source_install'    => '/admin_manual/installation/source_installation.html',
+    'admin-install'           => '/admin_manual/installation/index.html',
+    'admin-encryption'        => '/admin_manual/configuration/encryption_configuration.html',
+    'admin-external-storage'  => '/admin_manual/configuration/external_storage_configuration_gui.html',
 
-               'user-webdav'             => '/user_manual/files/files.html',
-               'user-manual'             => '/user_manual',
-               'user-sync-calendars'     => '/user_manual/pim/calendar.html#synchronising-calendars-with-caldav',
-               'user-sync-contacts'      => '/user_manual/pim/contacts.html#keeping-your-addressbook-in-sync',
-               'user-encryption'         => '/user_manual/files/encryption.html',
-               'user-trashbin'           => '/user_manual/files/deletedfiles.html',
-               'user-files'              => '/user_manual/files/index.html',
-               'user-versions'           => '/user_manual/files/versioncontrol.html'
-           );
+    'user-manual'             => '/user_manual',
+    'user-webdav'             => '/user_manual/files/files.html',
+    'user-sync-calendars'     => '/user_manual/pim/calendar.html#synchronising-calendars-with-caldav',
+    'user-sync-contacts'      => '/user_manual/pim/contacts.html#keeping-your-addressbook-in-sync',
+    'user-encryption'         => '/user_manual/files/encryption.html',
+    'user-trashbin'           => '/user_manual/files/deletedfiles.html',
+    'user-files'              => '/user_manual/files/index.html',
+    'user-versions'           => '/user_manual/files/versioncontrol.html'
+);
 
 ############# Do not edit below this line #################
 
