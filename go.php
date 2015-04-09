4c3440365449a39966285855f256138e2a20108a
Jan-Christoph Borchardt <hey@jancborchardt.net>
2015-04-09 10:29:42 +0200

    add documentation go links for hints in admin and sharing

--- go.php
+++ go.php
@@ -4,12 +4,14 @@
 ############## Adjust when editing docs ##################
 
 $mapping = array(
+    'admin-background-jobs'   => '/admin_manual/configuration_server/background_jobs_configuration.html',
     'admin-dir_permissions'   => '/admin_manual/installation/installation_wizard.html#setting-strong-directory-permissions',
     'admin-encryption'        => '/admin_manual/configuration_files/encryption_configuration.html',
     'admin-external-storage'  => '/admin_manual/configuration_files/external_storage_configuration_gui.html',
     'admin-install'           => '/admin_manual/installation/index.html',
     'admin-ldap'              => '/admin_manual/configuration_user/user_auth_ldap.html',
     'admin-provisioning-api'  => '/admin_manual/configuration_user/user_provisioning_api.html',
+    'admin-sharing-federated' => '/admin_manual/configuration_files/federated_cloud_sharing_configuration.html',
     'admin-source_install'    => '/admin_manual/installation/source_installation.html',
     'admin-backup'            => '/admin_manual/maintenance/backup.html',
     'admin-monitoring'        => '/admin_manual/operations/considerations_on_monitoring.html',
@@ -23,6 +25,7 @@ $mapping = array(
     'user-encryption'         => '/user_manual/files/encrypting_files.html',
     'user-files'              => '/user_manual/files/index.html',
     'user-manual'             => '/user_manual',
+    'user-sharing-federated'  => '/user_manual/files/federated_cloud_sharing.html',
     'user-sync-calendars'     => '/user_manual/pim/calendar.html#synchronizing-calendars-using-caldav',
     'user-sync-contacts'      => '/user_manual/pim/contacts.html#synchronizing-address-books',
     'user-trashbin'           => '/user_manual/files/deleted_file_management.html',
