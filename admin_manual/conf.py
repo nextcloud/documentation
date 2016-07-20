e82cfc62a4d1d0edada9898a06acdb9a6b807cb2
Lukas Reschke <lukas@statuscode.ch>
2016-07-20 23:26:57 +0200

    Version 10

--- admin_manual/conf.py
+++ admin_manual/conf.py
@@ -44,9 +44,9 @@ master_doc = 'contents'
 # built documents.
 #
 # The short X.Y version.
-version = '9.1'
+version = '10'
 # The full version, including alpha/beta/rc tags.
-release = '9.1'
+release = '10'
 
 # General information about the project.
 project = u'ownCloud %s Server Administration Manual' % (version)
