cbd58c39ebcbb9f2bbb39b9891f0d9dab26f5a4e
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2017-10-25 08:11:44 +0200

    failed rebase fix for float limit on design manual
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- developer_manual/conf.py
+++ developer_manual/conf.py
@@ -183,7 +183,7 @@ latex_elements = {
 #'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
-#'preamble': '',
+'preamble': '\extrafloats{100}\maxdeadcycles=500',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
