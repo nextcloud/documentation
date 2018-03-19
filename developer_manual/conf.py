0d1fef576081a5203866d1d36f907548a4fd3c1b
Juliane Holzt <juliane@holzt.de>
2018-03-20 00:34:32 +0100

    Fix latexpdf generation for developer_manual
    PDF creation of developer_manual via 'latexpdf' target fails because
    uses a unicode character (U+274C) which pdflatex does not recognize.
    Add Declaration of this character to Preamble via conf.py

--- developer_manual/conf.py
+++ developer_manual/conf.py
@@ -179,7 +179,7 @@ latex_elements = {
 #'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
-#'preamble': '',
+'preamble': '\DeclareUnicodeCharacter{274C}{\sffamily X}',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
