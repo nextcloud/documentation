3146fff5bbc8f0c6ea1f2bb178eaeabc5a830453
Carl Schwan <carl@carlschwan.eu>
2022-05-19 18:11:49 +0200

    Add redirection for admin delegation
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

--- go.php
+++ go.php
@@ -40,7 +40,8 @@ $mapping = array(
     'admin-trusted-domains'   => '/admin_manual/installation/installation_wizard.html#trusted-domains',
     'admin-update'            => '/admin_manual/maintenance/update.html',
     'admin-warnings'          => '/admin_manual/configuration_server/security_setup_warnings.html',
-    
+    'admin-delegation'        => '/admin_manual/configuration_server/admin_delegation_configuration.html',
+
     'developer-code-integrity'=> '/developer_manual/app/code_signing.html',
     'developer-manual'        => '/developer_manual',
     'developer-theming'       => '/developer_manual/core/theming.html',
