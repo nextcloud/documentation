694e7e20b9cb834797a29df79c18d9bfb8210353
Joas Schilling <coding@schilljs.com>
2023-05-22 11:37:00 +0200

    chore: Fix master branch after stable27 branch-off
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- conf.py
+++ conf.py
@@ -36,8 +36,8 @@ html_logo = "../_shared_assets/static/logo-white.png"
 rst_epilog =  '.. |version| replace:: %s' % version
 
 # building the versions list
-version_start = 24		# THIS IS THE SUPPORTED VERSION NUMBER
-version_stable = 26		# INCREASE THIS NUMBER TO THE LATEST STABLE VERSION NUMBER
+version_start = 25		# THIS IS THE SUPPORTED VERSION NUMBER
+version_stable = 27		# INCREASE THIS NUMBER TO THE LATEST STABLE VERSION NUMBER
 
 # Also search for "TODO ON RELEASE" in the rst files
 
