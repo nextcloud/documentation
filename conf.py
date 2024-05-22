23c8ebdf302315f2a4f4ccff1ad78a4b71061812
Richard Steinmetz <richard@steinmetz.cloud>
2024-05-22 16:22:14 +0200

    feat(docs): Add a "copy" button to code blocks
    Co-authored-by: Josh Richards <josh.t.richards@gmail.com>
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- conf.py
+++ conf.py
@@ -8,7 +8,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, os.path.abspath(dir_path + '/_ext'))
 now = datetime.datetime.now()
 
-extensions = ['sphinx_rtd_theme']
+extensions = ['sphinx_rtd_theme', 'sphinx_copybutton']
 
 # General information about the project.
 copyright = str(now.year) + ' Nextcloud GmbH'
