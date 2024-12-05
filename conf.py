e5fbbb24de797fd245a5227f5abeacae5c9b6900
Arthur Schiwon <blizzz@arthur-schiwon.de>
2024-12-05 18:23:54 +0100

    build: 28 entered EOL
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- conf.py
+++ conf.py
@@ -37,7 +37,7 @@ html_logo = "../_shared_assets/static/logo-white.png"
 rst_epilog =  '.. |version| replace:: %s' % version
 
 # building the versions list
-version_start = 28		# THIS IS THE OLDEST SUPPORTED VERSION NUMBER
+version_start = 29		# THIS IS THE OLDEST SUPPORTED VERSION NUMBER
 
 						# THIS IS THE VERSION THAT IS MAPPED TO https://docs.nextcloud.com/server/stable/
 version_stable = 30		# CHANGING IT MUST RESULT IN A CHANGE OF THE SYMLINK ON THE LIVE SERVER
