1b94fcc9586826f76e66cac2665d99ecc32585e4
skjnldsv <skjnldsv@protonmail.com>
2026-04-01 11:22:33 +0200

    fix: apply fixes from reviews
    Signed-off-by: skjnldsv <skjnldsv@protonmail.com>

--- build/build-index.php
+++ build/build-index.php
@@ -9,6 +9,11 @@ rsort($branches, SORT_NUMERIC);
 // We added this script when we had only 12 and above.
 $branches = array_filter($branches, fn($b) => $b >= 12);
 
+if (empty($branches)) {
+	fwrite(STDERR, "Error: No valid stable branches were provided. Please pass at least one numeric branch >= 12.\n");
+	exit(1);
+}
+
 // Generate current development version section
 $sections = [generate_section(($branches[0] + 1), 0)];
 
@@ -32,6 +37,12 @@ $index = str_replace(
 $index = str_replace(
 	'<!-- SERVER LEGACY BLOCK -->',
 	implode("\n", $legacy),
+	$index
+);
+
+$index = str_replace(
+	'<!-- CURRENT_YEAR -->',
+	date('Y'),
 	$index
 );
 
