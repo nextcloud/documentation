9639428f731c8cccfab019783f61c77b2202d556
skjnldsv <skjnldsv@protonmail.com>
2026-06-16 09:24:53 +0200

    fix: add display_version, update version_stable/start for current state
    The cherry-pick missed the display_version definition entirely, causing
    a NameError on any build. Add it with GITHUB_REF-based stable branch
    detection so HTML builds show the correct version number.
    
    Update version_stable from 32 to 34 (current highest released stable)
    and version_start from 31 to 32 (oldest documented) so the version
    picker reflects the current state of all branches.
    
    Signed-off-by: skjnldsv <skjnldsv@protonmail.com>

--- conf.py
+++ conf.py
@@ -10,10 +10,16 @@ now = datetime.datetime.now()
 
 os.environ["READTHEDOCS"] = "True"
 
-extensions = ['sphinx_rtd_theme', 'sphinx_rtd_dark_mode', 'sphinx_copybutton']
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinx_rtd_dark_mode',
+    'sphinx_copybutton',
+    'sphinxcontrib.mermaid',
+    'notfound.extension',
+]
 
 # General information about the project.
-copyright = str(now.year) + ' Nextcloud GmbH'
+copyright = '2016-' + str(now.year) + ' Nextcloud GmbH and Nextcloud contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -25,22 +31,46 @@ version = 'latest'
 # Can be overridden via DOCS_RELEASE env var (used for PDF builds to show the actual version number)
 release = os.environ.get('DOCS_RELEASE', version)
 
+# Ensure release is either "latest", or a digit (for stable versions)
+if not (release == 'latest' or release.isdigit()):
+	raise ValueError("Invalid release version: %s. Must be 'latest' or a digit." % release)
+
+# Print the version being built in a clear way in the logs
+width = 60
+msg = " Building documentation for version: %s " % release
+print("\n\n" + "#" * width)
+print("#" + msg.center(width - 2, "#") + "#")
+print("#" * width + "\n\n")
+
 # RTD theme options
 html_theme_options = {
-    'logo_only': True,
-    'style_external_links': True,
-    'display_version': False,
+	'logo_only': True,
+	'navigation_with_keys': True,
+	'style_external_links': True,
+	'version_selector': False,
 }
 
 # relative path to subdirectories
 html_logo = "../_shared_assets/static/logo-white.png"
 
-# substitutions go here
-rst_epilog =  '.. |version| replace:: %s' % version
+# disable including the reST sources in HTML builds (in _sources/) (default is True)
+html_copy_source = False
 
 # building the versions list
-version_start = 28		# THIS IS THE SUPPORTED VERSION NUMBER
-version_stable = 29		# INCREASE THIS NUMBER TO THE LATEST STABLE VERSION NUMBER
+# Update version_start when the lowest stableNN branch is deleted (version goes EoL).
+# Update version_stable when a new NC release ships (highest stableNN branch added).
+version_start = 32		# oldest documented version
+
+						# latest released stable — CHANGING IT MUST RESULT IN A CHANGE OF THE SYMLINK ON THE LIVE SERVER
+version_stable = 34		# mapped to https://docs.nextcloud.com/server/stable/
+
+import re as _re
+_stable_branch = _re.match(r'refs/heads/stable(\d+)$', os.environ.get('GITHUB_REF', ''))
+display_version = (
+    release if release != 'latest'                   # PDF/ePub builds (DOCS_RELEASE set)
+    else _stable_branch.group(1) if _stable_branch   # HTML builds on stableNN branches
+    else str(version_stable + 1)                     # HTML builds on master
+)
 
 # Also search for "TODO ON RELEASE" in the rst files
 
@@ -75,7 +105,6 @@ html_context = {
 	'current_version': version,
 	'display_version': display_version,
 	'READTHEDOCS': True,
-	'extra_css_files': ['_static/custom.css'],
 
 	# force github plugin
 	'display_github': True,
@@ -84,6 +113,12 @@ html_context = {
 	# If current version is an int, use the stablexxx branches, otherwise, edit on master
 	'theme_vcs_pageview_mode': 'edit/%s/' % github_branch, # to be completed by each individual conf.py
 }
+
+html_static_path = ['_static']
+# Extra CSS relative to html_static_path
+html_css_files = [
+	'custom.css'
+]
 
 edit_on_github_project = 'nextcloud/documentation'
 edit_on_github_branch = 'master'
@@ -105,3 +140,25 @@ if (version.isdigit() and version < version_start):
 default_dark_mode = False
 
 latex_engine = "xelatex"
+
+# -- Options for sphinx-notfound-page extension -----------------------------------
+# https://github.com/readthedocs/sphinx-notfound-page
+
+# content context passed to the 404 template
+notfound_context = {
+    "title": "404 Page Not Found",
+    "body": """
+<h1>Page Not Found</h1>
+<h2>Sorry, we can't seem to find the page you're looking for.</h2>
+<h6>Error code: 404</h6>
+
+<h3>Here are some alternatives:</h3>
+<ol>
+  <li>Try using the search box.</li>
+  <li>Check the content menu on the side of this page.</li>
+  <li>Regroup at our <a href="/">documentation homepage.</a></p></li>
+</ol>
+""",
+}
+
+notfound_urls_prefix = None
